f9efbaf1d51d5f7b6a1cbb23f5911cc7df11c194
Anthony Grimes <i@raynes.me>
2011-10-14 02:41:14 -0500

    Update dependencies and move to 1.3

--- project.clj
+++ project.clj
@@ -1,9 +1,3 @@
-(defproject clj-config "0.1.0-SNAPSHOT"
+(defproject clj-config "0.2.0"
   :description "A very simple configuration library that uses Clojure datastructures for configuration."
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-		     [swank-clojure "1.2.0-SNAPSHOT"]
-		     [leiningen/lein-swank "1.1.0"]
-		     [lein-search "0.3.0-SNAPSHOT"]
-		     [autodoc "0.7.0"]])
+  :dependencies [[clojure "1.3.0"]])
--- src/clj_config/core.clj
+++ src/clj_config/core.clj
@@ -1,5 +1,4 @@
 (ns clj-config.core
-  (:use [clojure.contrib.io :only [slurp*]])
   (:import java.io.File))
 
 (defn format-config 
@@ -15,23 +14,23 @@
 (defn write-config 
   "Writes the data to the configuration file. If format? is true, formats the file with
   format-config."
-  [new-info file & {:keys [format?] :or {format? false}}] 
+  [file new-info & {:keys [format?] :or {format? false}}] 
   (spit file (if format? (format-config new-info) (str new-info))))
 
 (defn get-key 
   "Get's a single key from the config file."
-  [key file]
+  [file key]
   (-> key ((read-config file))))
 
 (defn remove-key 
   "Removes a single key from the configuration file."
-  [key file & {:keys [format?] :or {format? false}}]
+  [file key & {:keys [format?] :or {format? false}}]
   (let [config (-> (read-config file) (dissoc key) str)]
     (write-config config file :format format?)))
 
 (defn set-key 
   "Sets the value of a key in a configuration file."
-  [key nval file & {:keys [format?] :or {format? false}}]
+  [file key nval & {:keys [format?] :or {format? false}}]
   (-> (read-config file) (assoc key nval) (write-config file)))
 
 (defn safely
